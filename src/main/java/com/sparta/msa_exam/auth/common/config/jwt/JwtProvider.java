288441669cd121a5c34867b654228e15a96c27fa
yurim <y1551463@gmail.com>
2024-12-01 19:53:43 +0900

    style (auth) : import 정리

--- src/main/java/com/sparta/msa_exam/auth/common/config/jwt/JwtProvider.java
+++ src/main/java/com/sparta/msa_exam/auth/common/config/jwt/JwtProvider.java
@@ -1,6 +1,5 @@
 package com.sparta.msa_exam.auth.common.config.jwt;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
